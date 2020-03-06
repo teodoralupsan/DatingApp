33e93f284f0dcccd6dc8ef471315cdb8ca7dfb5c
Teodora Lupsan <teodora_pintea_ramona@yahoo.com>
2020-03-06 11:33:32 +0200

    72. Entity Framework Relationships

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -1,13 +1,20 @@
-using DatingApp.API.Models;
+﻿using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DatingApp.API.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<Value> Values { get; set; }
+
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Photo> Photos { get; set; }
     }
 }
