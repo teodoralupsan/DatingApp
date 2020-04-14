b9b87eedab5961151e1d4212d3bd77a31f6e72ec
Teodora Lupsan <teodora_pintea_ramona@yahoo.com>
2020-04-14 10:26:02 +0300

    212,213. Ading a structural directive

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -35,6 +35,10 @@ import { ListResolver } from './_resolvers/lists.resolver';
 import { MessagesResolver } from './_resolvers/messages.resolver';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { HasRoleDirective } from './_directives/hasRole.directive';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
+import { AdminService } from './_services/admin.service';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -54,7 +58,10 @@ export function tokenGetter() {
       MemberEditComponent,
       PhotoEditorComponent,
       MemberMessagesComponent,
-      AdminPanelComponent
+      AdminPanelComponent,
+      HasRoleDirective,
+      UserManagementComponent,
+      PhotoManagementComponent
    ],
    imports: [
       BrowserModule,
@@ -90,7 +97,8 @@ export function tokenGetter() {
       ListResolver,
       AuthGuard,
       MessagesResolver,
-      PreventUnsavedChanges
+      PreventUnsavedChanges,
+      AdminService
    ],
    bootstrap: [
       AppComponent
